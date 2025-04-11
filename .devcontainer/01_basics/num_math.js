a17033927ca673803abc031d8db29d37cc504ee3
Rishabh Mishra <152280270+rishu449@users.noreply.github.com>
2025-04-11 04:30:26 +0000

    config done

---
+++ .devcontainer/01_basics/num_math.js
@@ -0,0 +1,31 @@
+/*const num=100
+//console.log(num)
+const score=new Number(200)
+console.log(score.toFixed(1));//output:200.0
+console.log(score.toFixed(2)); //output:200.00
+console.log(score.toString()); //convert datatype of score  into string
+
+const NUM=123.4572
+console.log(NUM.toPrecision(5))
+ const money=100000000000
+ console.log(money.toLocaleString('en-IN'));
+*/
+ //++++++++++++++++++++++++++++ Math +++++++++++++++++++++++++++++++++++++++++++
+
+ /*console.log(Math.abs(-5)); // converts values into +ve
+ console.log(Math.floor(10.34)); //Output:10
+ console.log(Math.ceil(10.34));//output:11
+ console.log(Math.round(123.64243));//output:124
+ console.log(Math.min(1,2,3,0)); 
+ console.log(Math.max(1,23,45,12));*/
+
+ console.log(Math.round(Math.random()*10)); //randomly display number between 0 and 1
+ 
+ // tast => to randomly print number between 10 and 20
+
+ const min=10
+ const max=20
+ console.log(Math.round(Math.random()*(max-min +1) +min))
+ 
+ 
+ 
