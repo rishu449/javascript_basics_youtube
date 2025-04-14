cf2b8ecf9c000dcecf64c9fc0d079b8848140a26
Rishabh Mishra <152280270+rishu449@users.noreply.github.com>
2025-04-14 06:37:59 +0000

    config done

---
+++ .devcontainer/01_basics/obj.js
@@ -0,0 +1,18 @@
+const mySym=Symbol('key1')
+const JsUser={
+    name:'Rishabh',
+    age:21,
+    address:'sitapur',
+    email:'abc@gmail.com',
+    [mySym]:'key1'              //we will write symbol key inside []
+   }
+//console.log( typeof JsUser[mySym]);
+//console.log(JsUser.age=22);
+
+//Object.freeze(JsUser)           //it will freeze th values inside the object
+
+JsUser.greet=function(){
+    console.log(`Hello ${this.name} , your age is ${this.age} ,you are from ${this.address}`);
+    
+}
+console.log(JsUser.greet());
