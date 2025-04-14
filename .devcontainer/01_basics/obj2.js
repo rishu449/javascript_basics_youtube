dcd21f53c8718f047f8dcd1e3bf01d46d80e7fff
Rishabh Mishra <152280270+rishu449@users.noreply.github.com>
2025-04-14 10:22:33 +0000

    config done

---
+++ .devcontainer/01_basics/obj2.js
@@ -0,0 +1,41 @@
+// singelton method or constructor method
+
+const tinder = new Object()
+tinder.name="rishabh"
+tinder.age=21
+console.log(tinder);
+
+
+//--------------------------------------------------------------------------
+
+const tinderUser={
+    username:{
+        fullname:{
+            firstName:"Rishabh",
+            lastName:"Mishra"
+        }
+    }
+}
+console.log(tinderUser.username.fullname);
+
+//--------------- merging two or more objects into one ------------------------------------
+
+const obj1={
+    1:'one',
+    2:'two'
+}
+const obj2={
+    3:'three',
+    4:'four'
+}
+const obj3={
+    5:'five',
+    6:'six'
+}
+const obj4={...obj1,...obj3,...obj2}
+console.log(obj4);
+
+//-----------------------------------------------------------------------------------------------------
+
+console.log(Object.keys(obj4));
+console.log(Object.values(obj4));
