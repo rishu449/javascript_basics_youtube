d8172ab19f0ecdc523ec803b24a6a5ae79375dd0
Rishabh Mishra <152280270+rishu449@users.noreply.github.com>
2025-04-16 07:39:50 +0000

    config done

--- .devcontainer/01_basics/scope.js
+++ .devcontainer/01_basics/scope.js
@@ -24,3 +24,59 @@ console.log(a);
 
 // NOTE => global variables can be access by both block scope  and global scope
 // but local variables can be only access inside their block scope
+
+//NOTE=> not recommended to use var while declaring variable because it can be access outside of the block  scope
+
+
+//=================================== PART 2 ===============================================================
+
+
+function one(){
+    const name='Rishabh'
+    function two(){
+        const city='sitapur'
+        console.log(name);      // Execute:name is a global variable and can be access inside block scope
+        
+    }
+    two()
+    console.log(city);         // Error:city is a local variable so it can be only used in its block 
+    
+}
+one()
+
+//---------------------------------------------------------------------------------------------
+ // SAME AS FUNCTION 
+if(true){
+    const username='rishu'
+    if(true){
+        const age=21
+        console.log(username,age);
+        
+    }
+    console.log(age);
+}
+
+
+
+//------------------------------------  INTRESTING ----------------------------------------------------------
+
+
+function myfun(n){
+    return n+1
+}
+myfun(4)
+
+// OR 
+myfun(4)
+function myfun(n){
+    return n+1
+}
+// here we can call the function  bafore or after its declaration
+// here both of the hoisting techniques are correct
+
+const myvar=function myfun2(m){
+    return m-1
+}
+myfun2(3)
+
+// here we can call the function after its declaration
