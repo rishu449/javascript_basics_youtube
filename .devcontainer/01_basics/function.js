e1464419919b76cdb684e9bdd930c767b10d900c
Rishabh Mishra <152280270+rishu449@users.noreply.github.com>
2025-04-16 06:05:14 +0000

    config done

---
+++ .devcontainer/01_basics/function.js
@@ -0,0 +1,9 @@
+function sum(x,y){
+    return x+y
+}
+ //console.log(sum(2,3))
+
+ function loginMsg(userName){
+        return `${userName} just logged in .`
+ }
+console.log(loginMsg())         // if we don't pass argument then it will return undefined value
