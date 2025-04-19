c0bf44a22a60ccf4d57943cd408d38ca8a500788
Rishabh Mishra <152280270+rishu449@users.noreply.github.com>
2025-04-19 09:51:38 +0000

    config done

---
+++ .devcontainer/control_flow/while.js
@@ -0,0 +1,22 @@
+  let i=0
+    while (i<=10) {
+        console.log(i);
+        i=i+1
+        
+    }
+
+const myArr=['batman','deadpool','ironman']
+let i=0
+while (i<myArr.length) {
+    console.log("i am ",myArr[i]);
+    
+    i=i+1
+}
+
+//do while loop
+
+let i=0
+do {
+    console.log(i);
+    i=i+2
+} while (i<=10);
